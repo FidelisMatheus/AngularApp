7945097d549c22fe7b654d944b727f2570b107e7
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-02-08 14:36:41 -0300

    Knowing first component

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //se transforma em uma tag html <app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo';
-}
+export class AppComponent { }
