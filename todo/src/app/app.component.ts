c0bb2b897c729e00cfe200d94df9c0752bb8fb08
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-02-09 10:49:38 -0300

    Created add command

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -23,9 +23,18 @@ export class AppComponent {
         Validators.required
       ])]
     });
-    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
-    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+  }
+
+  add() {
+    //ou: this.form.value => {title: 'tituto'}
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   remove(todo: Todo) {
